90ad1f1731d768bbcd8daad3030bd3927a63cad8
0b5vr <0b5vr@0b5vr.com>
2023-07-10 16:06:04 +0900

    chore: chore for Glitch.com

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    // disable linting, since it takes Glitch resources
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // disable type checking, since it takes Glitch resources
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
